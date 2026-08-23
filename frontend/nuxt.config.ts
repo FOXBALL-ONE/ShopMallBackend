cba59109cf0e0190631f916de65786ce0f6e2ae6
FOXBALL-ONE <admin@utf-8.fun>
2026-08-24 00:57:29 +0800

    更新SSR设置

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,9 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+  ssr: true,
   devtools: { enabled: true },
   css: ['~/assets/css/storefront.css'],
-
   modules: [
     '@nuxt/eslint',
     '@nuxt/image',
