d057f260ac0a95cb4c955a0bec8fbe697dd54c1b
FOXBALL-ONE <admin@utf-8.fun>
2026-08-24 20:15:12 +0800

    docs(order): 补充订单状态注释

--- src/main/kotlin/top/foxball/shopmall/entity/jdbc/OrderStatus.kt
+++ src/main/kotlin/top/foxball/shopmall/entity/jdbc/OrderStatus.kt
@@ -1,13 +1,22 @@
 package top.foxball.shopmall.entity.jdbc
 
 enum class OrderStatus {
+    /** 等待用户付款。 */
     PENDING_PAYMENT,
+    /** 已完成付款，等待后续处理。 */
     PAID,
+    /** 退款处理中。 */
     REFUNDING,
+    /** 已完成退款。 */
     REFUNDED,
+    /** 商品已发货。 */
     SHIPPED,
+    /** 商品已送达。 */
     DELIVERED,
+    /** 订单已完成。 */
     COMPLETED,
+    /** 订单已取消。 */
     CANCELLED,
+    /** 订单已逻辑删除。 */
     DELETED,
 }
